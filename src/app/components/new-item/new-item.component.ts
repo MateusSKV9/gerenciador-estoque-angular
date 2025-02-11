efc3e01852dea0668a61e7ca5358ba055db896e5
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-11 17:01:49 -0300

    feat: create and  style new-item component

---
+++ src/app/components/new-item/new-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-item.component.html',
+  styleUrl: './new-item.component.css',
+})
+export class NewItemComponent {}
