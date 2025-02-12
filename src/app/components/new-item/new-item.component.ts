d80027ccc34a3b5169c61c957ccbeb36e2d9c45b
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-12 16:23:53 -0300

    feat(new-item.component): implement reactive form

--- src/app/components/new-item/new-item.component.ts
+++ src/app/components/new-item/new-item.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../services/item.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Item } from '../../interfaces/item.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-new-item',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-item.component.html',
   styleUrl: './new-item.component.css',
 })
-export class NewItemComponent {}
+export class NewItemComponent implements OnInit {
+  form!: FormGroup;
+  idItem!: number;
+  item!: Item;
+
+  constructor(
+    private itemService: ItemService,
+    private fb: FormBuilder,
+    private activedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    let id = this.generateId();
+
+    const edit = this.activedRoute.snapshot.data['edit'];
+
+    if (edit) {
+      this.idItem = Number(this.activedRoute.snapshot.paramMap.get('id'));
+      this.item = this.itemService.getOne(this.idItem);
+    }
+
+    this.form = this.fb.group({
+      id: [this.idItem ? this.item.id : id],
+      name: [this.idItem ? this.item.name : ''],
+      quantity: [this.idItem ? this.item.quantity : null],
+      price: [this.idItem ? this.item.price : null],
+      category: [this.idItem ? this.item.category : ''],
+      description: [this.idItem ? this.item.description : ''],
+      createdAt: [this.idItem ? this.item.createdAt : ''],
+    });
+  }
+
+  onSubmit(): void {
+    const date = new Date();
+
+    this.form.get('craetedAt')?.setValue({
+      craetedAt: this.idItem ? this.item.createdAt : date,
+    });
+
+    const item = this.form.value as Item;
+
+    if (this.idItem) {
+      this.itemService.updateItem(this.idItem, item);
+    } else {
+      this.itemService.addItem(item);
+    }
+
+    this.form.reset();
+  }
+
+  generateId(): number {
+    let n1 = Math.floor(Math.random() * 10).toString();
+    let n2 = Math.floor(Math.random() * 10).toString();
+    let n3 = Math.floor(Math.random() * 10).toString();
+    let n4 = Math.floor(Math.random() * 10).toString();
+
+    let id: string = n1 + n2 + n3 + n4;
+    return Number(id);
+  }
+}
