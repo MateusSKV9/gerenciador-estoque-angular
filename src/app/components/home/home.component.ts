802ce5d72574b799f1696939b77f68b54c9f4b99
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-12 16:26:04 -0300

    feat: implement listing of recent and low-stock items

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../../interfaces/item.interface';
+import { ItemService } from '../../services/item.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  recentsItems!: Item[];
+  lowStockItems!: Item[];
+
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.getRecentItems();
+    this.getLowStockItems();
+  }
+
+  getRecentItems(): void {
+    this.recentsItems = this.itemService.getAll().reverse().slice(-0, 5);
+  }
+
+  getLowStockItems(): void {
+    this.lowStockItems = this.itemService
+      .getAll()
+      .filter((item) => item.quantity <= 10);
+  }
+}
