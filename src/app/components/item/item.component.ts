da4255d551ac483698696f23ed06cdcf6e550ea9
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-12 16:12:52 -0300

    feat: create e style item component

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../../interfaces/item.interface';
+import { ItemService } from '../../services/item.service';
+import { ActivatedRoute, Route, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css',
+})
+export class ItemComponent implements OnInit {
+  item!: Item;
+
+  constructor(
+    private itemService: ItemService,
+    private activedRoute: ActivatedRoute,
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.activedRoute.snapshot.paramMap.get('id'));
+    this.item = this.itemService.getOne(id);
+  }
+
+  onDeleteItem(item: Item): void {
+    this.itemService.deleteItem(item);
+    this.route.navigate(['/items']);
+  }
+}
